8ca4aec28c8590622f2c99ecc6a5a70d136bb50d
derpston <derpston@example.com>
2015-10-21 03:28:38 +0100

    Hitting everything with the autopep8 stick! Ha!

--- tests/test_me7.py
+++ tests/test_me7.py
@@ -1,10 +1,10 @@
 from unittest import TestCase
 import me7
 
+
 class TestECUExists(TestCase):
     """A pointless test, intended to test the test infrastructure and
     provide an example to build on."""
 
     def test_ecu_class_exists(self):
         self.assertTrue(getattr(me7, 'ECU'))
-
